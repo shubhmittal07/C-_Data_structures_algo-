aca5bedeb40f40f48096034fa6d246d67f77b097
Darkslayer07 <shubhmittal297@gmail.com>
2021-06-04 18:49:39 +0530

    Implementation of Vector

---
+++ README.md
@@ -0,0 +1,12 @@
+#Vector Notes
+
+1)Syntax-> std::vector<type> name;
+
+2)Object type reference .vs. Pointer type reference---->
+
+Basically objecy type veertex is more optimal as it store data in line,
+
+as compared to pointer type data, which scatters data on heap,
+
+which makes it a bit slow for iterating through the vertex.
+
---
+++ Vector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+struct Vertex
+{
+    int x,y,z;
+};
+
+std::ostream& operator<<(std::ostream& stream,const Vertex& vertex)
+{
+    stream<<vertex.x<<" , "<<vertex.y<<" , "<<vertex.z;
+    return stream;
+}
+int main()
+{
+    std::vector<Vertex> vertices;
+    vertices.push_back({1,2,3});
+    vertices.push_back({4,5,6});
+    for(Vertex& v:vertices)
+    {
+        std::cout<<v<<std::endl;
+    }
+    return 0;
+}
