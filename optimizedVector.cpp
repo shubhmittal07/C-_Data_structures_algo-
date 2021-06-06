cae5e3ff1b6dc5ee9203c35acb2b73a5e78efb70
Darkslayer07 <shubhmittal297@gmail.com>
2021-06-06 15:40:05 +0530

    updated optimized vector 02

--- optimizedVector.cpp
+++ optimizedVector.cpp
@@ -26,8 +26,8 @@ int main()
 {
     std::vector<Vertex> vertices;
     vertices.reserve(3);
-    vertices.emplace_back(Vertex(1,2,3));
-    vertices.emplace_back(Vertex(4,5,6));
-    vertices.emplace_back(Vertex(4,5,6));
+    vertices.emplace_back(1,2,3);
+    vertices.emplace_back(4,5,6);
+    vertices.emplace_back(4,5,6);
     return 0;
 }
